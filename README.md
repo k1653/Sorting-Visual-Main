5d5d872c0f15679cc1e2a243abac27cbb16df1c9
prernapriya <38802922+k1653@users.noreply.github.com>
2022-01-15 20:00:16 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# sorting-visual
+https://sort-visible-by-avnish.herokuapp.com/
---
+++ sorting/src/components/molecules/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+          href="https://www.linkedin.com/in/prerna-priya-5b09b214a"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Prerna Priya
+        </a>
+        <section className="Footer__Items">
+        <a
+          href="https://github.com/k1653"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Git-Hub
+        </a>
+        </section>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
